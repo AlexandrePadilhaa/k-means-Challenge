eecd33bc1f576fb78960f26ec7add7281bcd1ab8
AlexandrePadilhaa <105119507+AlexandrePadilhaa@users.noreply.github.com>
2024-06-04 22:56:55 -0300

    correcoes main e funcao que cria datasets

--- k-means.py
+++ k-means.py
@@ -1,11 +1,23 @@
 import sys
+import numpy as np
+import json
 
+from kmeans_class import KMeansCluster
 
-from Kmeans_class import KMeansCluster
+#cria um dataset uniformemente distribuido
+def create_dataset(filename: str, num_points: int):
+    data = np.random.rand(num_points, 2) * 10
+
+    with open(filename, "w") as file:
+        json.dump(data.tolist(), file)
+
 
 def main():
+    #Criação de datasets para testes
+    #create_dataset("dados_1000.json", 1000)
+
     if len(sys.argv) != 4:
-        print("Estrutura: python k-means.py input_file k output_file") #ex:  k-means.py small_dataset.json 3 output.json
+        print("Estrutura: python k-means.py input_file k output_file") #ex: python  k-means.py small_dataset.json 3 output.json
         sys.exit(1)
 
     input_file = sys.argv[1]
@@ -16,19 +28,15 @@ def main():
 
     k_means_object = KMeansCluster(input_file,k)
 
-
     k_means_object.load_data()
     print("data:", k_means_object.data)
-
 
     k_means_object.initialize_centroids()
     print("centroids:", k_means_object.centroids)
 
- 
     k_means_object.assign_clusters()
     print("labels:", k_means_object.labels)
 
-    
     new_centroids = k_means_object.update_centroids()
     print("atualizados centroids:", new_centroids)
 
@@ -36,7 +44,7 @@ def main():
     print("centroids finais:", k_means_object.centroids)
 
     k_means_object.save_results(output_file)
-    print("Resultado salvos em:", output_file)
+    print("Resultado salvo em:", output_file)
 
 
 if __name__ == "__main__":
