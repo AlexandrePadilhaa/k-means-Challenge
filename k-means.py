a1b33966f02a11b55ca0d1b83a377f70b6eff4fb
AlexandrePadilhaa <105119507+AlexandrePadilhaa@users.noreply.github.com>
2024-06-05 20:55:19 -0300

    alteracoes classe e metodo run

--- k-means.py
+++ k-means.py
@@ -14,10 +14,10 @@ def create_dataset(filename: str, num_points: int):
 
 def main():
     #Criação de datasets para testes
-    #create_dataset("dados_1000.json", 1000)
+    create_dataset("dados_100000.json", 100000)
 
     if len(sys.argv) != 4:
-        print("Estrutura: python k-means.py input_file k output_file") #ex: python  k-means.py small_dataset.json 3 output.json
+        print("Estrutura: python k-means.py input_file k output_file") #ex: python  k-means.py dados_100000.json 100 output.json
         sys.exit(1)
 
     input_file = sys.argv[1]
@@ -26,26 +26,13 @@ def main():
 
     print("Arquivo de entrada: ",input_file," K: ",k," Arquivo de saida: ", output_file)
 
-    k_means_object = KMeansCluster(input_file,k)
-
-    k_means_object.load_data()
-    print("data:", k_means_object.data)
-
-    k_means_object.initialize_centroids()
-    print("centroids:", k_means_object.centroids)
-
-    k_means_object.assign_clusters()
-    print("labels:", k_means_object.labels)
-
-    new_centroids = k_means_object.update_centroids()
-    print("atualizados centroids:", new_centroids)
-
-    k_means_object.iterate()
-    print("centroids finais:", k_means_object.centroids)
-
-    k_means_object.save_results(output_file)
-    print("Resultado salvo em:", output_file)
-
+    try:
+        kmeans = KMeansCluster(input_file, k)
+        kmeans.run(output_file)
+        print(f"Resultados salvos em {output_file}")
+    except ValueError as e:
+        print(f"Erro: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
--- kmeans_class.py
+++ kmeans_class.py
@@ -6,6 +6,12 @@ class KMeansCluster:
 
     #Método construtor da classe
     def __init__(self, input_file: str, k: int, max_iterations: int = 300):
+
+        if k <= 0:
+            raise ValueError("O valor de k deve ser maior que 0.")
+        if max_iterations <= 0:
+            raise ValueError("O número máximo de iterações deve ser maior que 0.")
+    
         #atributos da classe
         self.input_file = input_file
         self.k = k
@@ -19,33 +25,42 @@ class KMeansCluster:
         try:
             with open(self.input_file, 'r') as file:
                 self.data = np.array(json.load(file))
+            if self.k <= 0 or self.k > len(self.data):
+                raise ValueError("O valor de k deve ser maior que 0 e menor ou igual ao número de pontos de dados.")
         except FileNotFoundError:
             print(f"Erro: O arquivo '{self.input_file}' não foi encontrado.")
             sys.exit(1)
         except json.JSONDecodeError:
             print(f"Erro: O arquivo '{self.input_file}' não está no formato JSON válido.")
             sys.exit(1)
+        except ValueError as e:
+            print(e)
+            sys.exit(1)
 
     #Inicialização centroides (aleatoriamente)
     def initialize_centroids(self):
-        np.random.seed(42)
+        np.random.seed(42) # Semente do gerador de números aleatórios
         indices = np.random.choice(len(self.data), self.k, replace=False)
         self.centroids = self.data[indices]
     
     def assign_clusters(self):
+        # Calcula as distâncias de cada ponto de dados aos centróides usando a distancia euclidiana
         distances = np.linalg.norm(self.data[:, np.newaxis] - self.centroids, axis=2)
+        # Atribui cada ponto ao cluster mais proximo
         self.labels = np.argmin(distances, axis=1)
     
     def update_centroids(self):
+        # Calcula os novos centróides como a média dos pontos em cada cluster
         new_centroids = np.array([self.data[self.labels == i].mean(axis=0) for i in range(self.k)])
         return new_centroids
     
     def iterate(self):
+        # Itera até atingir o número máximo de iterações ou ocorrer a convergência antecipada
         for _ in range(self.max_iterations):
-            old_centroids = np.copy(self.centroids)
-            self.assign_clusters()
-            self.centroids = self.update_centroids()
-            if np.allclose(old_centroids, self.centroids):
+            old_centroids = np.copy(self.centroids) # salva centroides anteriores
+            self.assign_clusters() # atribui cada ponto ao cluster mais proximo
+            self.centroids = self.update_centroids() # recalcula os centroides
+            if np.allclose(old_centroids, self.centroids): # verifica se o algoritimo atingiu a convergencia antecipada, quando os centroides não mudam singificativamente
                 break
     
     def save_results(self, output_file: str):
@@ -55,3 +70,10 @@ class KMeansCluster:
         }
         with open(output_file, 'w') as file:
             json.dump(results, file)
+
+    def run(self, output_file: str):
+       
+        self.load_data()
+        self.initialize_centroids()
+        self.iterate()
+        self.save_results(output_file)
